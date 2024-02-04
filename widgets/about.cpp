b7c6586a04628ed59319120fc6e459181beb730c
avantol <andy.vantol@gmail.com>
2024-02-04 13:03:57 -0700

    Build 165
    First alpha build. Link to Github in Help|About.

--- widgets/about.cpp
+++ widgets/about.cpp
@@ -15,7 +15,7 @@ CAboutDlg::CAboutDlg(QWidget *parent) :
 
   ui->labelTxt->setText ("<h2>" + QString {"WSJT-X v"
                                              + QCoreApplication::applicationVersion ()
-                                             + " " + revision ()}.simplified () + "</h2>"
+                                             + " " + revision ()} + " (<a href=\"https://github.com/avantol/WSJT-X_2.7.0\">WM8Q mods</a>)" + "</h2>"
     "<h3> In memory of G4WJS, Bill Somerville (1956-2021),<br />"
     "who gave so much of himself to the WSJT project. </h3>"
 
