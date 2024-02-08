5a43474ffe1b5bdff035891da51e5368ee46a0ae
avantol <andy.vantol@gmail.com>
2024-02-08 12:04:10 -0700

    Build 169
    added visible m_auto indicator

--- widgets/mainwindow.cpp
+++ widgets/mainwindow.cpp
@@ -2407,6 +2407,15 @@ void MainWindow::process_autoButton (bool checked)   //manually or by controller
   debugToFile(QString{"             checked:%1"}.arg(checked));
 
   m_auto = checked;
+
+  if (is_externalCtrlMode()) {
+    if (m_auto) {     //avt 2/8/24
+      tx_enable_label.setStyleSheet ("QLabel{background-color: #ff5050}");
+    } else {
+      tx_enable_label.setStyleSheet ("QLabel{background-color: #f0f0f0}");
+    }
+  }
+  
   m_maxPoints=-1;
   if (checked
       && ui->respondComboBox->isVisible () && ui->respondComboBox->currentText() != "CQ: None"
@@ -2883,6 +2892,11 @@ void MainWindow::createStatusBar()                           //createStatusBar
 
   statusBar ()->addPermanentWidget (&watchdog_label);
   update_watchdog_label ();
+
+  tx_enable_label.setAlignment (Qt::AlignHCenter);    //avt 2/8/24
+  tx_enable_label.setMinimumSize (QSize {0, 18});
+  tx_enable_label.setFrameStyle (QFrame::Panel | QFrame::Sunken);
+  statusBar()->addPermanentWidget (&tx_enable_label);
 }
 
 void MainWindow::setup_status_bar (bool vhf)
@@ -11310,6 +11324,7 @@ void MainWindow::initExternalCtrl()    //avt 12/5/20
     ui->respondComboBox->setVisible(false); //avt
     ui->respondComboBox->setCurrentIndex(0); //avt
    ui->pbBestSP->setEnabled(false);  //avt 2/28/21
+   tx_enable_label.setVisible(true);    //avt 2/8/24
   } else {
     if (ui->cbAutoSeq-isVisible()) ui->cbAutoSeq->setEnabled(true); //avt 11/22/20
     if (ui->txFirstCheckBox-isVisible()) ui->txFirstCheckBox->setEnabled(true);  //avt 2/1/24
@@ -11324,6 +11339,7 @@ void MainWindow::initExternalCtrl()    //avt 12/5/20
     m_txEnableClk = false;  //avt 1/28/24
     m_enableButtonNotify = true;  //avt 1/23/24
     m_listenMode = false; //avt 2/1/24
+    tx_enable_label.setVisible(false);    //avt 2/8/24
   }
 }
 
