cae8efdc79bdc1af853cfea3e7508c291422263b
avantol <andy.vantol@gmail.com>
2024-02-08 12:12:08 -0700

    test git

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -60,7 +60,7 @@ QString revision (QString const& scs_rev_string)
       result = revision_from_scs;
     }
 #endif
-  return "169" /*result.trimmed ()*/;   //tempOnly
+  return "169" /*result.trimmed ()*/;  //tempOnly
 }
 
 QString version (bool include_patch)
